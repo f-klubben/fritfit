0e9f95e65a94401de6073f5e4641d9c53d0615e0
Nicolaj Østerby Jensen <noje@cs.aau.dk>
2022-10-29 14:17:11 +0200

    fritfit

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const img = new Image();
 img.onload = () => {
-    setInterval(loop, 1);
+    setInterval(loop, 16); // 60 hz
+    setInterval(repickAnimation, 5 * 60 * 1000); // Change every 5 min
 }
 img.src = "fritfit.png"
 const animations = [
+    new BouncingDvd(),
     new Rain(),
 ]
 let needsReset = true;
 let curAnim = null;
-repickAnimation()
+repickAnimation();
 
 // ======= Loop =======
 function loop() {
@@ -28,5 +30,6 @@ function resizeCanvas() {
 
 function repickAnimation() {
     needsReset = true;
-    curAnim = animations[Math.floor(Math.random() * animations.length)]
+    curAnim = animations[Math.floor(Math.random() * animations.length)];
+    canvas.clearRect(0, 0, canvas.width, canvas.height);
 }
